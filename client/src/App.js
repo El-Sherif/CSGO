7170d36e4e2cb556e360e89ab4351029ea789a87
Omar-Ayman <o7a9a98@gmail.com>
2019-12-05 16:32:37 +0200

    modified card style

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 // import './App.css';
 import Homepage from './components/Homepage';
 // import 'bootstrap/dist/css/bootstrap.min.css';
--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {Navbar, Nav, Alert, Card, ListGroup, ListGroupItem} from 'react-bootstrap'
+import {Navbar, Nav, Alert, Card, ListGroup, ListGroupItem, Button, CardColumns, Row, CardDeck} from 'react-bootstrap'
 import axios from 'axios'
 
 export default class Homepage extends Component {
@@ -9,15 +9,33 @@ export default class Homepage extends Component {
     this.displayEvents = this.displayEvents.bind(this)
     this.displayPlaces = this.displayPlaces.bind(this)
     this.displayCaterings = this.displayCaterings.bind(this)
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this)
 
     this.state = {
       id: props.id,
       displaying: "events",
       events: null,
       places: null,
-      caterings: null
+      caterings: null,
+      width: 0,
+      height: 0
     }
-    this.displayEvents();
+    this.displayEvents()
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions()
+    window.addEventListener('resize', this.updateWindowDimensions)
+  }
+  
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions)
+  }
+  
+  updateWindowDimensions() {
+    this.state.width = window.innerWidth
+    this.state.height = window.innerHeight
+    this.setState({...this.state})
   }
 
   displayEvents = () => {
@@ -85,6 +103,7 @@ export default class Homepage extends Component {
               <Nav.Link onClick={this.displayPlaces}>Places</Nav.Link>
               <Nav.Link onClick={this.displayCaterings}>Caterings</Nav.Link>
             </Nav>
+            <Button varient="outline-warning">New Event</Button>
           </Navbar>
         </div>
 
@@ -92,18 +111,26 @@ export default class Homepage extends Component {
           {this.state.displaying === "events"
             ? !this.state.events || !this.state.events[0]
               ? <Alert variant="secondary">No events available</Alert>
-              : this.state.events
+              : <>
+                <h3 style={{color: "blue", margin: 20}}>Events</h3>
+                <CardDeck style={{margin: 20}}>{
+                this.state.events
                 .map((val, idx) =>
                   (<Card> <Card.Body>
                     <Card.Title>{val.type}</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted">from {val.time.start_time} to {val.time.end_time}</Card.Subtitle>
+                    <Card.Subtitle className="mb-2 text-muted" margin={20}>from {val.time.start_time} to {val.time.end_time}</Card.Subtitle>
                     <Card.Text>{val.description}</Card.Text>
                   </Card.Body> </Card>))
+                }</CardDeck>
+                </>
 
           : this.state.displaying === "places"
             ? !this.state.places || !this.state.places[0]
               ? <Alert variant="secondary">No palces available</Alert>
-              : this.state.places
+              : <>
+                <h3 style={{color: "blue", margin: 20}}>Places</h3>
+                <CardDeck style={{margin: 20}}>{
+                this.state.places
                 .map((val, idx) =>
                   (<Card> <Card.Body>
                     <Card.Title>{val.name}</Card.Title>
@@ -111,11 +138,16 @@ export default class Homepage extends Component {
                     <Card.Subtitle className="mb-2 text-muted">{val.pricePerHour} EGP/hour</Card.Subtitle>
                     <Card.Text>{val.location}</Card.Text>
                   </Card.Body> </Card>))
+              }</CardDeck>
+              </>
 
           : this.state.displaying === "caterings"
             ? !this.state.caterings || !this.state.caterings[0]
               ? <Alert variant="secondary">No caterings available</Alert>
-              : this.state.caterings
+              : <>
+                <h3 style={{color: "blue", margin: 20}}>Caterings</h3>
+                <CardDeck style={{margin: 20}}>{
+                this.state.caterings
                 .map((val, idx) =>
                   (<Card> <Card.Body>
                     <Card.Title>{val.name}</Card.Title>
@@ -126,6 +158,8 @@ export default class Homepage extends Component {
                           {val.menu.map((item, i) => (<ListGroupItem>{item.item}: {item.price} EGP</ListGroupItem>))}
                         </ListGroup>}
                   </Card.Body> </Card>))
+                }</CardDeck>
+                </>
             : <></>
           }
         </div>
