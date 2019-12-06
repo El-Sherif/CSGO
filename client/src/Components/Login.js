e4ff44a4807b6b35b5837ef608a4e2346128b90d
Zeyad Khattab <41998602+ZeyadKhattab@users.noreply.github.com>
2019-12-06 10:19:07 +0200

    added parts of login

---
+++ client/src/Components/Login.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
+import "../styles/Login.css";
+import axios from "axios";
+
+export default function Login(props) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  async function handleSubmit(event) {
+
+    event.preventDefault();
+    const body={email:email,password:password};
+    const user = await axios.post(`http://localhost:5000/api/users/login`,body);
+    console.log("herer");
+  }
+
+  return (
+    <div className="Login">
+      <form onSubmit={handleSubmit}>
+        <FormGroup controlId="email" bsSize="large">
+          <FormLabel>Email</FormLabel>
+          <FormControl
+            autoFocus
+            type="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </FormGroup>
+        <FormGroup controlId="password" bsSize="large">
+          <FormLabel>Password</FormLabel>
+          <FormControl
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            type="password"
+          />
+        </FormGroup>
+        <Button block bsSize="large" disabled={!validateForm()} type="submit">
+          Login
+        </Button>
+      </form>
+    </div>
+  );
+}
