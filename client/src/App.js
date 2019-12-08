bba3aedd355540015ce0cff6298eef6856a8d7dd
El-Sherif <36974023+El-Sherif@users.noreply.github.com>
2019-12-08 20:02:41 +0200

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 // import logo from './logo.svg';
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import Homepage from './components/Homepage';
-import NewEvent from './components/NewEvent';
+import Homepage from './Components/Homepage';
+import NewEvent from './Components/NewEvent';
 import Login from './Components/Login';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Register from './Components/Register';
 import ForgetPassword from './Components/ForgetPassword'
 import ReactDOM from 'react-dom'
-
+import parseJwt from './helpers/decryptAuthToken'
 import './App.css';
 
 function App() {
@@ -25,8 +25,13 @@ function App() {
       </div>
     </div> */}
       <div className="App">
-        <Route exact path="/" component={() => <Homepage id="5dd9eb048c4fe83a9c3c069e" />} />
-        <Route exact path="/newEvent" component={() => <NewEvent id="5dd9eb048c4fe83a9c3c069e" />} />
+        <Route exact path="/" component={() => (localStorage.getItem('jwtToken')) ?
+          <Homepage id={parseJwt(localStorage.jwtToken).id} /> :
+          <Homepage id="" />
+        } />
+        <Route exact path="/newEvent" component={() => (localStorage.getItem('jwtToken')) ?
+          <NewEvent id={parseJwt(localStorage.jwtToken).id} />
+          : <NewEvent id="" />} />
         <Route exact path="/register" component={Register} />
         {/* <Route exact path="/test" component={testt} /> */}
         <Route exact path="/login" component={Login} />
